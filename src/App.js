1922727fdbeaea0b1e3bdfa1426dd3154ee736df
MrLapcha <rkhem1514@gmail.com>
2024-01-26 22:52:17 +0545

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import {useState} from 'react';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import './index.css';
 
-function App() {
+
+const App=()=> {
+const [p, setP] = useState(0)
+const [t, setT] = useState(0)
+const [r, setR] = useState(0)
+const [si, setSi] = useState(0) 
+
+const Interest = ()=> {
+  setSi(p*t*r/100)
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="div">
+       <AppBar position="static">
+        <Toolbar>
+          
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+        Simple Interest
+          </Typography>
+        
+        </Toolbar>
+      </AppBar>
+      <br>
+      </br>
+      <TextField  onChange={(event)=> setP(event.target.value)} id="outlined-basic" label="Enter P" variant="outlined" />  <br></br> <br></br>
+      <TextField onChange={(event)=> setT(event.target.value)} id="outlined-basic" label="Enter T" variant="outlined" />  <br></br> <br></br>
+      <TextField onChange={(event)=> setR(event.target.value)} id="outlined-basic" label="Enter R" variant="outlined" />
+      <br></br>
+      <br></br>
+      <Button onClick={()=>Interest()} variant="contained">Calculate</Button>
+    
+      <Typography variant="h6" gutterBottom component='div'>
+       Simple interest is: {si}
+      </Typography>
     </div>
   );
 }
